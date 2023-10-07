c1e5d9eb669fb58275217ab940b8ae9cbf7af3a8
Davi Nunes <davi.nunes@gmail.com>
2023-10-07 13:34:26 -0300

    teste

--- git.php
+++ git.php
@@ -7,7 +7,7 @@
 		$mensagem = $_POST['msg'] ? $_POST['msg'] : "Atualização";
 		// echo $mensagem."<br/>";
 		$cmd = 'cd /var/www/html 2>&1; 
-		/usr/bin/git pull 2>&1;  
+		/usr/bin/git pull main 2>&1;  
 		/usr/bin/git add . 2>&1;  
 		/usr/bin/git commit -m "'.$mensagem.'"; 
 		/usr/bin/git push origin main 2>&1';
@@ -22,7 +22,7 @@
 		/usr/bin/git config credential.helper cache 2>&1;
 		'; 
 	// $cmd = '/usr/bin/git config --global --add safe.directory /var/www/html 2>&1'; 
-	// $cmd = 'whoami'; //
+	// $cmd = 'whoami'; ////
 
 	$output = shell_exec($cmd);
 	echo "<pre>".$output."</pre>";
