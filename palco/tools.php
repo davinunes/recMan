cb886292dfde0012df846d277e94eb57785e396e
Davi Nunes <davi.nunes@gmail.com>
2025-11-16 11:48:57 -0300

    backup

--- palco/tools.php
+++ palco/tools.php
@@ -5,7 +5,7 @@
 ?>
 <div class=' row'>
 <h3>Ferramentas do Desenvolvedor</h3>
-<a class='btn' href="json.php">Importar Planilha da Soluções</a>
+<a class='btn' href="json.php?importar_magnacom=1">Importar Planilha da Soluções</a>
 <a class='btn' target="_blank" href="backup.php">Backup</a>
 <a class='btn' target="_blank" href="git.php">GIT</a>
 <a class='btn' target="_blank" href="gmail/getToken.php">Novo Token Gmail</a>
