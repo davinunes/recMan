b53f53a1444ef592ca52bfb3569e374ba2b43038
Davi Nunes <davi.nunes@gmail.com>
2026-02-15 22:46:38 -0300

    update

--- palco/tools.php
+++ palco/tools.php
@@ -4,11 +4,14 @@
 
 ?>
 <div class=' row'>
-<h3>Ferramentas do Desenvolvedor</h3>
-<a class='btn' href="json.php?importar_magnacom=1">Importar Planilha da Soluções</a>
-<a class='btn' target="_blank" href="backup.php">Backup</a>
-<a class='btn' target="_blank" href="git.php">GIT</a>
-<a class='btn' target="_blank" href="gmail/getToken.php">Novo Token Gmail</a>
-<a class='btn' target="_blank" href="gmail/refresh.php">Atualizar Token Gmail</a>
-<a class='btn' target="_blank" href="tools/datasRetirada.php">Cadastrar datas de retiradas </a>
+<h3>Ferramentas</h3>
+<a class='btn' href="json.php?importar_magnacom=1">Importar Planilha da Soluções</a></br></br> 
+<a class='btn' target="_blank" href="backup.php">Backup</a></br></br> 
+<a class='btn' target="_blank" href="git.php">GIT</a></br></br> 
+<a class='btn' target="_blank" href="gmail/getToken.php">Novo Token Gmail</a></br></br> 
+<a class='btn' target="_blank" href="gmail/refresh.php">Atualizar Token Gmail</a></br></br> 
+<!-- <a class='btn' target="_blank" href="tools/datasRetirada.php">Cadastrar datas de retiradas </a></br></br> -->
+<a class='btn' target="_blank" href="regimento/">Pesquisar Regimento</a></br></br> 
+<a class='btn' target="_blank" href="ocorrenciasCondominioDigital/relatorio.php">Ocorrencias Condominio Digital</a></br></br> 
+<a class='btn' target="_blank" href="ocorrenciasCondominioDigital/quantitativos.php">Quantitativos</a></br></br> 
 </div>
