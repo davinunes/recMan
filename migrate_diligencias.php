a5f2a1f850176f51b4ef54a705f7d2cf3234e8ba
ilunne <davi.nunes@gmail.com>
2026-03-29 09:45:26 -0300

    cria php para migrate

---
+++ migrate_diligencias.php
@@ -0,0 +1,18 @@
+<?php
+require_once "classes/repositorio.php";
+require_once "vendor/autoload.php";
+use Minishlink\WebPush\VAPID;
+
+echo "<h3>Iniciando Migration para Web Push Notifications</h3>";
+
+$sql = file_get_contents("storage/migrations_20260329.sql");
+
+if (DBExecute($sql)) {
+    echo "<p style='color:green;'>1. Tabelas criadas ou ja existentes.</p>";
+} else {
+    echo "<p style='color:red;'>Erro ao criar tabelas.</p>";
+}
+
+
+echo "<h3>Migrate concluído com sucesso!</h3>";
+?>
