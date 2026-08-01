c1f1f6f0c3a040c022a84feb802ac8c481283368
ilunne <davi.nunes@gmail.com>
2026-08-01 10:04:01 -0300

    fix

--- migrates/migrate_vds_integration.php
+++ migrates/migrate_vds_integration.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/classes/repositorio.php";
+require_once __DIR__ . "/../classes/repositorio.php";
 
 echo "Iniciando migração de banco de dados para integração VDS (API v8)...\n";
 
